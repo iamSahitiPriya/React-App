5f1756b30e2527b36bf289750362afddf8eb003c
iamSahitiPriya <smssaddepalli@gmail.com>
2022-02-04 17:02:45 +0530

    final commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { getAllByText, render, screen,fireEvent } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom'
+import Form from "./components/Form";
+import Todo from "./components/Todo";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("basic rendering of the form page", ()=>{
+  it("button should not be disabled",()=>{
+    const {getByTestId} = render(<Form />);
+  expect(getByTestId("Add")).not.toHaveAttribute("disabled");
+  })
+});
+
+describe("basic rendering of the todo page",()=>{
+  it("delete button should not in disabled state",()=>{
+    const {getByTestId}=render(<Todo />);
+    expect(getByTestId("delete")).not.toHaveAttribute("disabled");
+  })
+});
+
+
+describe("the functionality of form page",()=>{
+  it("shoould Add button renders correctly",()=>{
+    const {getByTestId}=render(<Form label="Add"/>);
+    expect(getByTestId("Add")).toHaveTextContent("Add");
+  })
+  const mockedset=jest.fn();
+
+  it('should be able to type into input', () => {
+    render(
+        <Form
+            name={[]}
+            setName={mockedset}
+        />
+    );
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    fireEvent.click(inputElement)
+    fireEvent.change(inputElement, { target: { value: "Go get task" } })
+    expect(inputElement.value).toBe("Go get task");
+
+});
 });
